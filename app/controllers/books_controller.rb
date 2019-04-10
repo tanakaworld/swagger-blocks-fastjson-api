76de2ea1854203408660a31b98891940021451f6
tanakaworld <yutaro.tanaka.world@gmail.com>
2019-04-10 22:47:41 +0900

    feat: add image to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -70,6 +70,6 @@ class BooksController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def book_params
-    params.require(:book).permit(:title, :description)
+    params.require(:book).permit(:title, :description, :image)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  has_one_attached :image
 end
