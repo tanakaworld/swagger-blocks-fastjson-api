c66026141cba832fe85956c51ee54fc4e90a6e07
tanakaworld <yutaro.tanaka.world@gmail.com>
2019-06-23 01:04:16 +0900

    chore: prettify

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,19 +5,18 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import App from '../app.vue'
+import Vue from 'vue';
+import App from '../app.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const el = document.body.appendChild(document.createElement('hello'))
-    const app = new Vue({
-        el,
-        render: h => h(App)
-    })
+  const el = document.body.appendChild(document.createElement('hello'));
+  const app = new Vue({
+    el,
+    render: h => h(App)
+  });
 
-    console.log(app)
-})
-
+  console.log(app);
+});
 
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
@@ -31,7 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
 //   {{message}}
 //   <app></app>
 // </div>
-
 
 // import Vue from 'vue/dist/vue.esm'
 // import App from '../app.vue'
